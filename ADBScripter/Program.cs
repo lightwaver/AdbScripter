0c339bc049a885f81abc173bab4c37281c1a327c
Stefan M. Marek <stefan.marek@post.at>
2018-01-31 14:29:38 +0100

    Script Execution

--- ADBScripter/Program.cs
+++ ADBScripter/Program.cs
@@ -1,44 +1,149 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 
 namespace ADBScripter
 {
     class Program
     {
+        private const string settingsFile = "settings.json";
+        private static bool running = true;
+        private static Settings settings = new Settings();
+
         static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("ADB Scripter - 2018 - Stefan M. Marek");
+                Console.WriteLine();
+
+                settings = LoadSettings(settings);
+
+                Doit();
+
+                Console.WriteLine("press enter to exit...");
+                Console.ReadLine();
+                running = false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+
+        private static async void Doit()
+        {
+            var devices = new List<string>();
+            while (running)
+            {
+                try
+                {
+                    var newDevices = GetDeviceList();
+
+                    foreach (var newDevice in newDevices)
+                    {
+                        if (devices.Contains(newDevice)) continue;
+
+                        devices.Add(newDevice);
+                        Console.WriteLine("device added: " + newDevice);
+                        StartScript4Device(newDevice);
+                    }
+
+                    foreach (var device in devices.ToArray())
+                    {
+                        if (newDevices.Contains(device)) continue;
+
+                        devices.Remove(device);
+                        Console.WriteLine("device removed: " + device);
+                    }
+
+                    await Task.Delay(1000);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ops: " + ex);
+                }
+            }
+
+        }
+
+        private static Settings LoadSettings(Settings settings)
+        {
+            if (File.Exists(settingsFile))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(Settings));
+                using (var sr = new FileStream(settingsFile, FileMode.Open))
+                {
+                    settings = serializer.ReadObject(sr) as Settings;
+                }
+            }
+            return settings;
+        }
+
+        private static void StartScript4Device(string device)
         {
             ProcessStartInfo pi = new ProcessStartInfo()
             {
-                FileName ="adb\\adb.exe",
+                FileName = settings.ScriptToExecute,
+                Arguments = settings.ScriptArguments.Replace("%device%", device),
+                UseShellExecute = true
+            };
+            Process.Start(pi);
+        }
+
+        static List<string> GetDeviceList()
+        {
+            ProcessStartInfo pi = new ProcessStartInfo()
+            {
+                FileName = "adb\\adb.exe",
                 Arguments = "devices",
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
             };
             var p = Process.Start(pi);
 
-            while(!p.HasExited)
+            string device;
+            var newDevices = new List<string>();
+            while (!p.HasExited)
             {
                 var line = p.StandardOutput.ReadLine();
-                Console.WriteLine("op: " + line);
+
+                if (AnalyseLine(line, out device))
+                {
+                    newDevices.Add(device);
+                }
             }
 
             while (!p.StandardOutput.EndOfStream)
             {
                 var line = p.StandardOutput.ReadLine();
-                Console.WriteLine("op: " + line);
+                //Console.WriteLine("op: " + line);
 
-                AnalyseLine(line);
+                if (AnalyseLine(line, out device))
+                {
+                    newDevices.Add(device);
+                }
             }
-            Console.ReadLine();
 
+            return newDevices;
         }
 
-        private static void AnalyseLine(string line)
+        private static bool AnalyseLine(string line, out string device)
         {
+            device = null;
             line = line.Trim();
-            if (string.IsNullOrEmpty(line) || line == "List of devices attached") return;
-            
-
+            //if (string.IsNullOrEmpty(line) || line == "List of devices attached") return false;
+            if (line.EndsWith("device"))
+            {
+                device = line.Substring(0, line.IndexOf('\t'));
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ ADBScripter/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ADBScripter
+{
+    [DataContract]
+    public class Settings
+    {
+        public string AdbPath { get; set; } = "adb\\adb.exe";
+        public string DevicesArguments { get; set; } = "devices";
+
+        public string ScriptToExecute { get; set; } = "start.cmd";
+        public string ScriptArguments { get; set; } = "%device%";
+    }
+}
