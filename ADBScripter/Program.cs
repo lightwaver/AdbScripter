f4371e9f51739b4ccfa92f9369b8324d1de3e167
lightwaver <lightwaver@gmx.at>
2018-01-31 08:02:47 +0100

    Projektdateien hinzufügen.

---
+++ ADBScripter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ADBScripter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo pi = new ProcessStartInfo()
+            {
+                FileName ="adb\\adb.exe",
+                Arguments = "devices",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+            var p = Process.Start(pi);
+
+            while(!p.HasExited)
+            {
+                var line = p.StandardOutput.ReadLine();
+                Console.WriteLine("op: " + line);
+            }
+
+            while (!p.StandardOutput.EndOfStream)
+            {
+                var line = p.StandardOutput.ReadLine();
+                Console.WriteLine("op: " + line);
+
+                AnalyseLine(line);
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void AnalyseLine(string line)
+        {
+            line = line.Trim();
+            if (string.IsNullOrEmpty(line) || line == "List of devices attached") return;
+            
+
+        }
+    }
+}
